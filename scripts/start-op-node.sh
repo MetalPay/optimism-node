baabf48c8abc6eba6869cefa54cdba6b3a94d132
Glenn <705252+MlennGarien@users.noreply.github.com>
2024-07-17 09:02:21 +0200

    Update config

--- scripts/start-op-node.sh
+++ scripts/start-op-node.sh
@@ -12,13 +12,12 @@ export EXTENDED_ARG="${EXTENDED_ARG:-} --rollup.config=/chainconfig/rollup.json"
 # Start op-node.
 exec op-node \
   --l1=$OP_NODE__RPC_ENDPOINT \
+  --l1.trustrpc \
+  --l1.beacon=$OP_NODE__L1_BEACON \
   --l2=http://op-geth:8551 \
   --rpc.addr=0.0.0.0 \
   --rpc.port=9545 \
   --l2.jwt-secret=/shared/jwt.txt \
-  --l1.trustrpc \
-  --l1.rpckind=$OP_NODE__RPC_TYPE \
-  --l1.beacon=$OP_NODE__L1_BEACON \
   --metrics.enabled \
   --metrics.addr=0.0.0.0 \
   --metrics.port=7300 \
